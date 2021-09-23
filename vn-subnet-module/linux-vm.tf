dad153f520e03e99606ca6a4b846910c0ba7224b
ifs-vohelk <voshitha.hettiarachchi@ifsworld.com>
2021-09-23 16:21:35 +0530

    Added Linux VMs

--- vn-subnet-module/linux-vm.tf
+++ vn-subnet-module/linux-vm.tf
@@ -2,6 +2,12 @@ resource "azurerm_network_interface" "networkInterface" {
   name                = "example-nic"
   location            = var.location
   resource_group_name = var.resourceName
+
+    ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sb1.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
